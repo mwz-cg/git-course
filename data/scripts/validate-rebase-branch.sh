b517775475eaf9403aabba05dc2cf8728c35de20
Matthieu Wipliez <matthieu@codingame.com>
2017-06-15 15:49:12 +0200

    update verification script for rebase

--- data/scripts/validate-rebase-branch.sh
+++ data/scripts/validate-rebase-branch.sh
@@ -1,17 +1,12 @@
 # { autofold
 #!/bin/bash
 
-echo add something awesome > /tmp/rebase-branch-expected
-echo remove lapin >> /tmp/rebase-branch-expected
-echo add master feature >> /tmp/rebase-branch-expected
-echo add me >> /tmp/rebase-branch-expected
-
 function checkHistoryRebased {
   FINISHED=0
   while [ $FINISHED -eq 0 ]; do
     if [ -e .git ]; then
       git log --format=%s new-feature > /tmp/rebase-branch-actual
-      if cmp -s /tmp/rebase-branch-actual /tmp/rebase-branch-expected; then
+      if cmp -s /tmp/rebase-branch-actual /project/target/rebase-log; then
         echo "TECHIO> success true"
         FINISHED=1
       fi
