ed80eb9783be99b4ef328e3b7d86506a6b315fc0
Matthieu Wipliez <matthieu@codingame.com>
2017-06-15 10:52:45 +0200

    update script with grep test

--- data/scripts/validate-tag-update.sh
+++ data/scripts/validate-tag-update.sh
@@ -5,8 +5,12 @@ function checkTagExists {
   FINISHED=0
   showkey -a
   while [ $FINISHED -eq 0 ]; do
+    if grep "tag '.*' already exists" lapin; then
+      echo "TECHIO> success true"
+      FINISHED=1
+    fi
     sleep 1
-  done 
+  done
 }
 echo "TECHIO> terminal -s 'bash -c \"bash -i 2>&1 | tee lapin; exit $?\"' -i 'cd repositories/unique-tag/; mv .gitexample .git'"
 
